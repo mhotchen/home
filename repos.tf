b189585e9f162d96e201eefd92fb96723147acb9
Matthew Hotchen <m@mhn.me>
2018-09-09 18:22:00 +0100

    Adds mhn.me repo

--- repos.tf
+++ repos.tf
@@ -3,3 +3,12 @@ module "infrastructure" {
   name        = "Infrastructure"
   description = "General infrastructure provisioning"
 }
+
+module "mhn_me" {
+  source      = "./repo"
+  name        = "mhn.me"
+  description = "My personal homepage"
+}
+
+output "infrastructure_repo" { value = "${module.infrastructure.repository_url}\n" }
+output "mhn_me_repo" { value = "${module.mhn_me.repository_url}\n" }
