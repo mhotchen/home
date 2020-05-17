96d347c2089ee0d176e4a67a51cd774fe15d1e28
Matthew Hotchen <m@mhn.me>
2020-05-17 21:37:10 +0100

    Tidies mhn.me build process

--- mhn.me/build/handler.ts
+++ mhn.me/build/handler.ts
@@ -1,4 +1,4 @@
-import { Context, Handler, SNSEvent } from 'aws-lambda'
+import { SNSHandler } from 'aws-lambda'
 import { CodeCommit, S3 } from 'aws-sdk'
 import { contentType } from 'mime-types'
 
@@ -7,7 +7,7 @@ interface CommitRef {
   readonly ref: string
 }
 
-export const build: Handler = async (event: SNSEvent, context: Context) => {
+export const build: SNSHandler = async event => {
   const commitRef: CommitRef = JSON.parse(event.Records[0].Sns.Message).Records[0].codecommit.references[0]
   if (commitRef.ref !== 'refs/heads/master') {
     return
@@ -17,25 +17,24 @@ export const build: Handler = async (event: SNSEvent, context: Context) => {
   const codeCommit = new CodeCommit
   const s3 = new S3
 
-  const folder = codeCommit
+  const folder = await (codeCommit
     .getFolder({
       repositoryName,
       folderPath: '/mhn.me/public',
       commitSpecifier: commitRef.commit,
     })
     .promise()
+  )
 
-  const fileUploads = (await folder).files
-    .map(async ({ blobId, relativePath }) =>
-      s3.putObject({
-          Bucket: 'mhn-me',
-          Key: relativePath,
-          ContentType: contentType(relativePath) || 'application/octet',
-          Body: (await codeCommit.getBlob({ repositoryName, blobId }).promise()).content,
-          CacheControl: 'max-age=3155760000', // 100 years
-        })
-        .promise()
-    )
-
-  await Promise.all(fileUploads)
+  await Promise.all(folder.files
+      .map(async ({ blobId, relativePath }) =>
+          s3.putObject({
+              Bucket: 'mhn-me',
+              Key: relativePath,
+              ContentType: contentType(relativePath) || 'application/octet',
+              Body: (await codeCommit.getBlob({ repositoryName, blobId }).promise()).content,
+              CacheControl: 'max-age=3155760000', // 100 years
+          })
+              .promise()
+      ))
 }
