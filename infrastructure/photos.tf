48b5f1ef11c7e0ee5a2d1dba14afbb129c6b9a1a
Matthew Hotchen <m@mhn.me>
2020-08-11 20:52:15 +0100

    Adds private s3 bucket

--- infrastructure/photos.tf
+++ infrastructure/photos.tf
@@ -1,6 +1,16 @@
-resource "aws_s3_bucket" "photos" {
+resource "aws_s3_bucket" "public" {
   bucket = "c54e80e7-fc58-48db-ac87-5fef67069a2d"
   acl    = "public-read"
+  region = "eu-west-2"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket" "private" {
+  bucket = "c7171850-8f93-4d04-afe7-1fd248cb253b"
+  acl    = "private"
   region = "eu-west-2"
 
   lifecycle {
